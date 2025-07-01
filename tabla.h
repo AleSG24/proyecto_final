9c8fb3f6b2d8de02ff8176dcaac5cbb039d0e904
AleSG24 <alejandrosanchezg25@gmail.com>
2025-06-30 18:58:40 -0600

    tablas finales

--- tabla.h
+++ tabla.h
@@ -1,5 +1,8 @@
 //header para tablero
 #include <SDL2/SDL_render.h>
+#include <stdbool.h>
+
+extern bool game_over;
 
 
 struct Tablero{
@@ -12,6 +15,11 @@ void liberar_interfaz(struct Tablero *tablero, int exit_status);
 bool sdl_inicio_tablero(struct Tablero *tablero);
 bool load_tablero(struct Tablero *tabero);
 void dibujar_celdas();
-
+void colocar_contador(SDL_Renderer *renderer);
+void colocar_temporizador(SDL_Renderer *renderer);
+void iniciar_temp();
+void actualizar_temp();
+void gameover_check(struct Tablero *tablero, SDL_Renderer *renderer);
+void texture_winner(struct Tablero *tablero, SDL_Renderer *renderer);
 
 
--- tablero.c
+++ tablero.c
@@ -8,8 +8,12 @@
 
 #define WINDOW_TITLE "buscaminas"
 #define SCREEN_WIDTH 1100
-#define SCREEN_HEIGHT 900
+#define SCREEN_HEIGHT 1000
 #define IMAGE_FLAGS IMG_INIT_PNG
+#define TOTAL_MINAS 10
+
+bool game_over = false;
+
 
 struct Tablero{
 	SDL_Window *window;
@@ -18,15 +22,27 @@ struct Tablero{
 };
 
 
-
-
-
-
-
+time_t tiempo_inicio;
+int segundos_transcurridos = 0;
 
 bool sdl_inicio_tablero(struct Tablero *tablero);
 bool load_tablero(struct Tablero *tablero);
 void liberar_interfaz(struct Tablero *tablero, int exit_status);
+
+
+void iniciar_temp() {
+    tiempo_inicio = time(NULL);
+}
+
+void actualizar_temp(){
+
+	
+    if (!game_over && !winner) {
+        segundos_transcurridos = (int)difftime(time(NULL), tiempo_inicio);
+    
+	}
+
+}
 
 void liberar_interfaz(struct Tablero *tablero, int exit_status){
 	SDL_DestroyTexture(tablero->background);
@@ -77,5 +93,128 @@ bool load_tablero(struct Tablero *tablero){
 	return false;
 }
 
+void colocar_contador(SDL_Renderer *renderer){
+	if (TTF_Init() == -1){
+		printf("Error al inicializar %s\n", TTF_GetError());
+		exit(1);
+	}
 
+	TTF_Font *fuente = TTF_OpenFont("fuentes/RubikSprayPaint-Regular.ttf", 24);
+	SDL_Color color = {0, 0, 0};
+	char texto[12];
+	snprintf(texto, sizeof(texto), "%d", TOTAL_MINAS - banderas_utilizadas);
+
+
+
+    SDL_Surface* superficie = TTF_RenderText_Solid(fuente, texto, color);
+    if (!superficie) {
+        printf("Error al renderizar texto: %s\n", TTF_GetError());
+        return;
+    }
+
+    SDL_Texture* textura = SDL_CreateTextureFromSurface(renderer, superficie);
+    if (!textura) {
+        printf("Error al crear textura: %s\n", SDL_GetError());
+        SDL_FreeSurface(superficie);
+        return;
+    }
+
+    SDL_Rect destino = {
+        .x = 300,
+	.y = 150,
+	.w = 90,
+	.h = 90
+    };
+
+    SDL_RenderCopy(renderer, textura, NULL, &destino);
+
+
+    SDL_FreeSurface(superficie);
+    SDL_DestroyTexture(textura);
+
+	TTF_CloseFont(fuente);
+	TTF_Quit();
+
+	
+}
+
+void colocar_temporizador(SDL_Renderer *renderer){
+	if (TTF_Init() == -1){
+		printf("Error al inicializar %s\n", TTF_GetError());
+		exit(1);
+	}
+
+	TTF_Font *fuente = TTF_OpenFont("fuentes/Bitcount.ttf", 24);
+	SDL_Color color = {255, 0, 0, 255};
+	char texto[12];
+	snprintf(texto, sizeof(texto), "%d", segundos_transcurridos);
+
+
+
+
+
+    SDL_Surface* superficie = TTF_RenderText_Solid(fuente, texto, color);
+    if (!superficie) {
+        printf("Error al renderizar texto: %s\n", TTF_GetError());
+        return;
+    }
+
+    SDL_Texture* textura = SDL_CreateTextureFromSurface(renderer, superficie);
+    if (!textura) {
+        printf("Error al crear textura: %s\n", SDL_GetError());
+        SDL_FreeSurface(superficie);
+        return;
+    }
+
+    SDL_Rect destino = {
+        .x = 800,
+	.y = 150,
+	.w = 90,
+	.h = 90
+    };
+
+	if (game_over || winner){
+		SDL_Rect destino = {
+			.x = 510,
+			.y = 750,
+			.w = 90,
+			.h = 90
+	
+		};
+		SDL_RenderCopy(renderer, textura, NULL, &destino);
+		SDL_FreeSurface(superficie);
+    		SDL_DestroyTexture(textura);
+
+		TTF_CloseFont(fuente);
+		TTF_Quit();
+
+
+	}
+	else{
+
+   	 	SDL_RenderCopy(renderer, textura, NULL, &destino);
+
+
+   	 	SDL_FreeSurface(superficie);
+    		SDL_DestroyTexture(textura);
+
+		TTF_CloseFont(fuente);
+		TTF_Quit();
+
+	}
+	
+}
+
+void gameover_check(struct Tablero *tablero, SDL_Renderer *renderer){
+	if (game_over){
+		tablero->background = IMG_LoadTexture(tablero->renderer, "images/gameover.png");
+	
+	}
+}
+
+void texture_winner(struct Tablero *tablero, SDL_Renderer *renderer){
+	if (winner){
+		tablero->background = IMG_LoadTexture(tablero->renderer, "images/winner.png");
+	}
+}
 
